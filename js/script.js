cede721571f2df5e33e3ef4256d076d0084e844a
Prakash-Banjade <87601648+Prakash-Banjade@users.noreply.github.com>
2022-07-03 01:13:04 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+console.log("This is news site created by Prakash Banjade")
+
+let xhr = new XMLHttpRequest();
+let newsSrc = 'in'
+let apiKey = 'bff321d664ea4cb3a095bbd2716a33d0'
+let newsContainer = document.getElementById('newsContainer');
+let leftNews = document.getElementById("ln");
+let rightNews = document.getElementById('rn');
+let updateTime = document.getElementById('updateTime');
+
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?country=${newsSrc}&apiKey=${apiKey}`, true);
+
+xhr.onload = function() {
+    if (this.status == 200) {
+        let json = JSON.parse(this.responseText);
+        updateTime.innerText = json.articles[0].publishedAt;
+
+        let leftNewsHtml = '';
+        let rightNewsHtml = '';
+        json.articles.forEach(function(element, index) {
+            leftNewsHtml += `<div class="news1" id="${index}">
+            <span>${element.title}</span>
+            <img src="${element.urlToImage}">
+            <span class="readMore">Read More <a href="${element.url}">Here!</a></span>
+        </div>`;
+            rightNewsHtml += `<li><a href="#${index}">${element.title}</a></li>`
+        });
+
+        leftNews.innerHTML = leftNewsHtml;
+        leftNews.children[0].children[2].style.display = "none";
+        rightNews.innerHTML = `<ul>${rightNewsHtml}</ul>`;
+
+        leftNews.children[0].addEventListener('click', () => {
+            location.href = `${json.articles[0].url}`;
+        })
+
+
+
+    } else {
+        newsContainer.innerHTML = `<div style="font-size: 3rem; margin-top: 2rem;">The server is down for some reason!<br><span style="font-size:1.5rem;">Please try again in a while.</span></div>`;
+    }
+}
+
+
+
+xhr.send();
